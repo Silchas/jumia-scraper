e92a51d8e5e4e54ee989a646023c2ce1354d86c7
Silchas <Cartharoncrust@gmail.com>
2023-07-27 09:58:16 +0300

    implemented multiple page scrapping

--- jumia.py
+++ jumia.py
@@ -2,27 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url = 'https://www.jumia.co.ke/mlp-top-deals/'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-
-products = soup.find_all('article', class_='prd _fb _p col c-prd')
 data = []
+page = 1
+while page != 6:
+    url = f'https://www.jumia.co.ke/mlp-top-deals/?page={page}'
+    page = page + 1
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-for product in products:
-    product_name = product.find('h3', class_='name').text.strip()
-    
-    price_element = product.find('div', class_='prc')
-    price = price_element.text.strip() if price_element else None
-    
-    discount_element = product.find('div', class_='bdg _dsct _sm')
-    discount = discount_element.text.strip() if discount_element else None
-    
-    reviews_element = product.find('div', class_='rev')
-    reviews = reviews_element.text.strip() if reviews_element else None
-    
-    
-    data.append({'product name': product_name, 'price': price, 'discount': discount, 'reviews': reviews})
+    products = soup.find_all('article', class_='prd _fb _p col c-prd')
+
+    for product in products:
+        product_name = product.find('h3', class_='name').text.strip()
+        
+        price_element = product.find('div', class_='prc')
+        price = price_element.text.strip() if price_element else None
+        
+        discount_element = product.find('div', class_='bdg _dsct _sm')
+        discount = discount_element.text.strip() if discount_element else None
+        
+        reviews_element = product.find('div', class_='rev')
+        reviews = reviews_element.text.strip() if reviews_element else None
+        
+        
+        data.append({'product name': product_name, 'price': price, 'discount': discount, 'reviews': reviews})
 
 with open('jumia.csv', 'w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=['product name', 'price', 'discount', 'reviews'])
